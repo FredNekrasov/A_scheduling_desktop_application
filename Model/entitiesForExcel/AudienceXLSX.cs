90e0589e41c17d6cf1a3a70df1237b6f7f85ff90
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 15:46:13 +0300

    creating objects for easy excel file generation

---
+++ Model/entitiesForExcel/AudienceXLSX.cs
@@ -0,0 +1,9 @@
+﻿namespace Model.entitiesForExcel;
+
+public class AudienceXLSX
+{
+    public string AudienceType { get; set; } = string.Empty;
+    public int SeatsNumber { get; set; }
+    public int StudentNumber { get; set; }
+    public string AudienceNumber { get; set; } = string.Empty;
+}
---
+++ Model/entitiesForExcel/WeekXLSX.cs
@@ -0,0 +1,8 @@
+﻿namespace Model.entitiesForExcel;
+
+public class WeekXLSX
+{
+    public int Year { get; set; }
+    public string Semester { get; set; } = string.Empty;
+    public int WeekNumber { get; set; }
+}
