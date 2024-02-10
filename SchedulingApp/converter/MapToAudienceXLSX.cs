b6ce9e0e7e25e22dfbb7b31c330e3c1e15974e7e
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 15:57:05 +0300

    creating mappers to generate xlsx objects

---
+++ SchedulingApp/converter/MapToAudienceXLSX.cs
@@ -0,0 +1,18 @@
+﻿using Model.entities.room;
+using Model.entitiesForExcel;
+
+namespace SchedulingApp.converter;
+
+public class MapToAudienceXLSX
+{
+    public static AudienceXLSX ToAudienceXLSX(Audience audience)
+    {
+        return new AudienceXLSX
+        {
+            AudienceNumber = audience.AudienceNumber,
+            SeatsNumber = audience.SeatsNumber,
+            AudienceType = audience.AudienceType.TypeName,
+            StudentNumber = audience.StudentNumber
+        };
+    }
+}
---
+++ SchedulingApp/converter/MapToWeekXLSX.cs
@@ -0,0 +1,19 @@
+﻿using Model.entities.date;
+using Model.entitiesForExcel;
+
+namespace SchedulingApp.converter;
+
+public class MapToWeekXLSX
+{
+    public static WeekXLSX ToWeekXLSX(Week week)
+    {
+        string semester;
+        if (week.Semester.IsEven == true) semester = "1st semester"; else semester = "2ond semester";
+        return new WeekXLSX
+        {
+            WeekNumber = week.WeekNumber,
+            Semester = semester,
+            Year = week.Semester.Year
+        };
+    }
+}
