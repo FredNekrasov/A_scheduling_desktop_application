9f3bc59c18bf21fa78adcf86ffa8deffc0a6303c
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 13:39:17 +0300

    creating windows for adding and editing data

---
+++ SchedulingApp/view/saveData/date/WeekWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SchedulingApp.view.saveData.date
+{
+    /// <summary>
+    /// Interaction logic for WeekWindow.xaml
+    /// </summary>
+    public partial class WeekWindow : Window
+    {
+        public WeekWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
