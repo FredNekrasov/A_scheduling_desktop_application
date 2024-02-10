b057349c0f282f2c2fd1ff161c9e2f59e9772414
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 08:08:23 +0300

    creating repository to work with the restful api

---
+++ Model/repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Model.repositories;
+
+public interface IRepository<T>
+{
+    Task<IEnumerable<T>?> Read();
+    Task Create(T entity);
+    Task Delete(int id);
+    Task Update(T entity, int id);
+}
---
+++ Model/repositories/implementation/Repository.cs
@@ -0,0 +1,20 @@
+﻿using Model.service;
+using Newtonsoft.Json;
+
+namespace Model.repositories.implementation;
+
+public class Repository<T>(ISchedulingAPI schedulingAPI, string apiName) : IRepository<T>
+{
+    private readonly HttpClient api = schedulingAPI.GetHttp();
+    private readonly string endpoint = apiName + "/";
+    public async Task Create(T entity) => await api.PostAsJsonAsync(apiName, entity);
+    public async Task Delete(int id) => await api.DeleteAsync(endpoint + id);
+    public async Task<IEnumerable<T>?> Read()
+    {
+        var response = await api.GetStringAsync(apiName);
+        var items = JsonConvert.DeserializeObject<IEnumerable<T>>(response);
+        if (items == null) return null;
+        return items;
+    }
+    public async Task Update(T entity, int id) => await api.PutAsJsonAsync(endpoint + id, entity);
+}
