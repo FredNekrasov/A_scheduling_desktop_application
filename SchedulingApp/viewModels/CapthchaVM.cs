375f0cdc88cce12876a1af0956f355959e22af06
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 10:48:11 +0300

    creating a ViewModel for captcha generation

---
+++ SchedulingApp/viewModels/CapthchaVM.cs
@@ -0,0 +1,24 @@
+﻿using Model.captcha;
+
+namespace SchedulingApp.viewModels
+{
+    public class CapthchaVM
+    {
+        private readonly GenerateCaptcha generateCaptcha;
+        private string password = string.Empty;
+        private string font = string.Empty;
+        public string Captcha { get => password; }
+        public string Font { get => font; }
+        public CapthchaVM(GenerateCaptcha _generateCaptcha)
+        {
+            generateCaptcha = _generateCaptcha;
+            ReCreateCaptcha();
+        }
+        public void ReCreateCaptcha()
+        {
+            string[] result = generateCaptcha.GenerateCaptchaCode().Split(',');
+            password = result[0];
+            font = result[1];
+        }
+    }
+}
