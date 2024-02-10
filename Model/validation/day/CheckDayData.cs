0452bf1ff328c3541c0d7b90ee8e4addd6ff6a2c
FredNekrasov <frednekrasov49@gmail.com>
2024-02-10 08:54:38 +0300

    creating checks for the day

---
+++ Model/validation/day/CheckDayData.cs
@@ -0,0 +1,19 @@
+﻿namespace Model.validation.day;
+
+public class CheckDayData : IDayOfWeekValidation
+{
+    public string ValidateDayOfWeek(string dayOfWeek)
+    {
+        return dayOfWeek switch
+        {
+            "1" or "понедельник" or "пн" => "Monday",
+            "2" or "вторник" or "вт" => "Tuesday",
+            "3" or "среда" or "ср" => "Wednesday",
+            "4" or "четверг" or "чт" => "Thursday",
+            "5" or "пятница" or "пт" => "Friday",
+            "6" or "суббота" or "сб" => "Saturday",
+            "7" or "воскресенье" or "вс" => "Sunday",
+            _ => "data is empty or incorrect"
+        };
+    }
+}
---
+++ Model/validation/day/IDayOfWeekValidation.cs
@@ -0,0 +1,6 @@
+﻿namespace Model.validation.day;
+
+public interface IDayOfWeekValidation
+{
+    string ValidateDayOfWeek(string dayOfWeek);
+}
