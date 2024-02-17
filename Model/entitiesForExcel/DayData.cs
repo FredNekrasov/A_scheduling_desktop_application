3024a21d16db739397eb670ce3e0c48c6fc89b0e
FredNekrasov <frednekrasov49@gmail.com>
2024-02-17 11:47:06 +0300

    creating a data class to display day data

---
+++ Model/entitiesForExcel/DayData.cs
@@ -0,0 +1,19 @@
+﻿using Model.entities;
+using Model.entities.date;
+
+namespace Model.entitiesForExcel
+{
+    public class DayData
+    {
+        public int ID { get; set; }
+        public string DayOfWeek { get; set; } = string.Empty;
+        public PairEntity? Pair1 { get; set; }
+        public PairEntity? Pair2 { get; set; }
+        public PairEntity? Pair3 { get; set; }
+        public PairEntity? Pair4 { get; set; }
+        public PairEntity? Pair5 { get; set; }
+        public PairEntity? Pair6 { get; set; }
+        public PairEntity? Pair7 { get; set; }
+        public Week Week { get; set; }
+    }
+}
