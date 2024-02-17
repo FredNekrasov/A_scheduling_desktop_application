c68b8a07fc2d5e5c88380c5a1bb3ee6d8f28a33e
FredNekrasov <frednekrasov49@gmail.com>
2024-02-17 20:56:03 +0300

    refactoring a DayWindow and a SaveDayViewModel

--- SchedulingApp/view/saveData/date/DayWindow.xaml.cs
+++ SchedulingApp/view/saveData/date/DayWindow.xaml.cs
@@ -11,15 +11,14 @@ namespace SchedulingApp.view.saveData.date
 {
     public partial class DayWindow : Window
     {
-        private readonly ISaveVM<DayEntity> _saveVM;
+        private readonly ISaveVM<DayData> _saveVM;
         private readonly IBasicVM<Week> _weeksBasicVM = new WeekVM();
         private readonly IBasicVM<PairEntity> _pairsBasicVM = new PairVM();
         private DayData entity = new();
         public DayWindow()
         {
             InitializeComponent();
-            IDayOfWeekValidation dayOfWeekValidation = new CheckDayData();
-            _saveVM = new SaveDay(dayOfWeekValidation);
+            _saveVM = new SaveDay();
             SetData();
             DataContext = entity;
         }
@@ -36,7 +35,6 @@ namespace SchedulingApp.view.saveData.date
             PairsLV6.ItemsSource = _pairsBasicVM.List;
             PairsLV7.ItemsSource = _pairsBasicVM.List;
         }
-        private async Task<string> Save(DayEntity item) => await _saveVM.SaveAsync(item);
         private void GoBackClick(object sender, RoutedEventArgs e)
         {
             ViewListWindow viewList = new();
@@ -45,7 +43,7 @@ namespace SchedulingApp.view.saveData.date
         }
         private async void SaveClick(object sender, RoutedEventArgs e)
         {
-            var result = await Save(GetEntity());
+            var result = await _saveVM.SaveAsync(entity);
             if (result != "Ok")
             {
                 MessageBox.Show(result, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -53,27 +51,6 @@ namespace SchedulingApp.view.saveData.date
             }
             MessageBox.Show("Запись успешно создана", result, MessageBoxButton.OK, MessageBoxImage.Asterisk);
             GoBackClick(sender, e);
-        }
-        private DayEntity GetEntity()
-        {
-            DayEntity dayEntity = new()
-            {
-                ID = entity.ID,
-                DayOfWeek = entity.DayOfWeek,
-                Pair1 = GetID(entity.Pair1),
-                Pair2 = GetID(entity.Pair2),
-                Pair3 = GetID(entity.Pair3),
-                Pair4 = GetID(entity.Pair4),
-                Pair5 = GetID(entity.Pair5),
-                Pair6 = GetID(entity.Pair6),
-                Pair7 = GetID(entity.Pair7),
-                Week = entity.Week
-            };
-            return dayEntity;
-        }
-        private static int? GetID(PairEntity? pairEntity)
-        {
-            if (pairEntity == null) return null; else return pairEntity.PairID;
         }
     }
 }
--- SchedulingApp/viewModels/saveVMImplementation/date/SaveDay.cs
+++ SchedulingApp/viewModels/saveVMImplementation/date/SaveDay.cs
@@ -1,4 +1,5 @@
-﻿using Model.entities.date;
+﻿using Model.entities;
+using Model.entities.date;
 using Model.repositories;
 using Model.validation.day;
 using SchedulingApp.stupidDI;
@@ -6,18 +7,36 @@ using System.Text;
 
 namespace SchedulingApp.viewModels.saveVMImplementation.date;
 
-public class SaveDay(IDayOfWeekValidation dayOfWeekValidation) : ISaveVM<DayEntity>
+public class SaveDay : ISaveVM<DayData>
 {
     private readonly IRepository<DayEntity> _repository = RepositoryModule<DayEntity>.GetRepository("Days");
-    private readonly IDayOfWeekValidation _dayValidation = dayOfWeekValidation;
-    public async Task<string> SaveAsync(DayEntity obj)
+    private readonly IDayOfWeekValidation dayOfWeekValidation = new CheckDayData();
+    public async Task<string> SaveAsync(DayData obj)
     {
         StringBuilder errors = new();
-        string result = _dayValidation.ValidateDayOfWeek(obj.DayOfWeek);
+        var entity = GetEntity(obj);
+        string result = dayOfWeekValidation.ValidateDayOfWeek(entity.DayOfWeek);
         if (result == "data is empty or incorrect") errors.AppendLine(result);
-        if (obj.Week == null) errors.AppendLine("Номер недели не выбран");
+        if (entity.Week == null) errors.AppendLine("Номер недели не выбран");
         if (errors.Length > 0) return errors.ToString();
-        if (obj.ID == 0) await _repository.Create(obj); else await _repository.Update(obj, obj.ID);
+        if (entity.ID == 0) await _repository.Create(entity); else await _repository.Update(entity, entity.ID);
         return "Ok";
     }
+    private static DayEntity GetEntity(DayData data)
+    {
+        return new DayEntity()
+        {
+            ID = data.ID,
+            DayOfWeek = data.DayOfWeek,
+            Pair1 = GetID(data.Pair1),
+            Pair2 = GetID(data.Pair2),
+            Pair3 = GetID(data.Pair3),
+            Pair4 = GetID(data.Pair4),
+            Pair5 = GetID(data.Pair5),
+            Pair6 = GetID(data.Pair6),
+            Pair7 = GetID(data.Pair7),
+            Week = data.Week
+        };
+    }
+    private static int? GetID(PairEntity? pairEntity) => pairEntity?.PairID;
 }
